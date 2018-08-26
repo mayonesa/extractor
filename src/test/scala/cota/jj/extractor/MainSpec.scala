ba56192be09537a79dc66064c5592fbe051833fe
John Jimenez <mayonesa.cosmica@gmail.com>
2018-08-25 21:27:14 -0400

    moved test resources to its proper location

--- src/test/scala/cota/jj/extractor/MainSpec.scala
+++ src/test/scala/cota/jj/extractor/MainSpec.scala
@@ -2,12 +2,14 @@ package cota.jj.extractor
 
 import org.scalatest.FlatSpec
 
+import java.io.File
 import io.Source.fromFile
 import play.api.libs.json.Json.{ parse => json }
 
 class MainSpec extends FlatSpec {
-  private val outFile = "out.test.json"
-  Main.main(Array("in.test.json", outFile))
+  private val path = "src/test/resources/"
+  private val outFile = path + "out.test.json"
+  Main.main(Array(path + "in.test.json", outFile))
   private val outJson = json(fromFile(outFile).getLines.mkString)
   
   "app" should "return proper days" in {
